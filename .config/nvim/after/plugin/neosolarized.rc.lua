5ff7e34591e78ed8583ea0ab7b4e0f1e40cd1e60
minhnvc <milihouse.nguyen@gmail.com>
2023-02-01 12:17:57 +0700

    feat(theme): update to onedark with high contrast

--- .config/nvim/after/plugin/neosolarized.rc.lua
+++ .config/nvim/after/plugin/neosolarized.rc.lua
@@ -1,5 +1,35 @@
 require('onedark').setup {
   style = 'dark',
-  transparent = true
+  transparent = true,
+  colors = {
+    bblack = "#0e1013",
+		bg0 = "#1f2329",
+		bg1 = "#282c34",
+		bg2 = "#30363f",
+		bg3 = "#323641",
+		bg_d = "#181b20",
+		bg_blue = "#61afef",
+		bg_yellow = "#e8c88c",
+		fg = "#eeeeee",
+		purple = "#cd72ed",
+		green = "#acec87",
+		orange = "#eaa969",
+		blue = "#4fa6ed",
+		yellow = "#f2d762",
+		cyan = "#54d0e2",
+		red = "#ff6c7c",
+		grey = "#7e879d",
+		light_grey = "#b3bfd3",
+		dark_cyan = "#266269",
+		dark_red = "#8b3434",
+		dark_yellow = "#835d1a",
+		dark_purple = "#7e3992",
+		diff_add = "#272e23",
+		diff_delete = "#2d2223",
+		diff_change = "#172a3a",
+		diff_text = "#274964",
+  }
 }
 require('onedark').load()
+
+
